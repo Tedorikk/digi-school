477cb7048bff57e98d4d738163a642c9b82aeee8
Tedrick <tedrikex@gmail.com>
2025-02-03 12:35:00 +0700

    Minor Fix (Database fix - tenant database not used)

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Events\EventServiceProvider;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\SuperAdminPanelProvider::class,
     App\Providers\Filament\TenantPanelProvider::class,
+    EventServiceProvider::class,
+    RouteServiceProvider::class,
     App\Providers\TenancyServiceProvider::class,
 ];
